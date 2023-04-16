702288cc19c8e1bacb3a3879575a55baae11cff2
wy <189039786106@163.com>
2023-04-16 13:58:42 +0800

    fix: 增加husky

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   // 肩头函数只有一个参数时可以忽略括号
-  arrowParents: 'avoid',
+  arrowParens: 'avoid',
   // 括号内部不要出现空格
   bracketSpacing: true,
   // 行结束符使用 Unix 格式
   endOfLine: 'lf',
   // true： put > on the last line instead of at a new line
-  jsxBracketSameLine: false,
+  bracketSameLine: false,
   // 行宽
   printWidth: 100,
   // 换行方式
@@ -18,7 +18,7 @@ module.exports = {
   // 缩进
   tabWidth: 2,
   // 使用tab缩进
-  useTab: false,
+  useTabs: false,
   // 后置逗号，多行对象、数组在最后一行增加逗号
   trailingComma: 'es5',
   parser: 'typescript',
